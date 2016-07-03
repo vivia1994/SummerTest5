1b6dbeb3e417dcbaecff407336fb12911c2295b2
Vivia Lei <leiwei183@163.com>
2016-07-03 19:38:18 +0800

    农场的边长

--- Dispsy loves flowers/源.cpp
+++ Dispsy loves flowers/源.cpp
@@ -1,9 +1,65 @@
 #include<iostream>
-
+#include<cstdio>
+#include<cstring>
 using namespace std;
 
-int main()
-{
+char mp[1005][1005];
+int l[1005][1005], r[1005][1005], u[1005][1005], d[1005][1005], a[1005][1005];
 
+int main(void)
+ {
+	
+	int t; 
+	cin>>t; 
+	while (t--)
+	{
+		int max = 0; 
+		int m, n; 
+		cin >> m >> n; 
+		memset(a, 0, sizeof(a)); 
+		memset(l, 0, sizeof(l)); 
+		memset(r, 0, sizeof(r)); 
+		memset(d, 0, sizeof(d)); 
+		memset(u, 0, sizeof(u)); 
+		for (int i = 1; i <= m; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				cin>>mp[i][j]; 
+
+				if (mp[i][j - 1] == '.')
+					l[i][j] = l[i][j - 1] + 1;
+				else
+					l[i][j] = 0;
+				if (mp[i - 1][j] == '.')
+					u[i][j] = u[i - 1][j] + 1;
+				else
+					u[i][j] = 0;
+				if (mp[i][j] == '.')
+					a[i][j] += l[i][j] + u[i][j];
+			}
+		}//
+		
+		for (int i = m; i >= 1; i--)
+			for (int j = n; j >= 1; j--)
+			{
+				
+				if (mp[i][j + 1] == '.')
+				{
+					r[i][j] = r[i][j + 1] + 1;
+				}
+				else
+					r[i][j] = 0; 
+				if (mp[i + 1][j] == '.')
+					d[i][j] = d[i + 1][j] + 1;
+				else
+					d[i][j] = 0; 
+				if (mp[i][j] == '.')
+					a[i][j] += r[i][j] + d[i][j];
+				if (max < a[i][j])
+					max = a[i][j];
+			}
+		printf("%d\n", max); 
+	}
 	return 0;
 }
--- 农场的边长/源.cpp
+++ 农场的边长/源.cpp
@@ -1,88 +1,55 @@
-/*Description
-      飞哥最近 得到 了一大块 n*m 的土地 ，他想在这块土地 上建一个正方 形的农场 ，但是 这块土 地有些地方 被大石头 占着，无法 使用 ，于是 飞哥想来问你 在这块土地 上能建成的农场 的最大 边 长为多少 ？
-
-Input
-第一个数T表示数组组数
-每组 数据 一开 始有两个 整数 n和 m，表示 土地 的长和宽。
-接下 来是个 n*m 的矩阵 ，元素 只有 0 和 1 ，0表示 这块地方 被石头 占着。1表示 可以 使用 。
-
-m,n<=1000
-
-Output
-对每组数据 输出 一行 ，表示 农场 最大 的边长 。
-
-Sample Input
-1
-4 4
-0 1 1 1
-1 1 1 0
-0 1 1 0
-1 1 0 1
-Sample Output
-2*/
-
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-#define ll long long
+int m[1000][1000];
+int r, c;
 
-struct Matrix
+int CanBeBigger(pair<int, int> p, int size)
 {
-	ll a[6][6];
-};
-
-Matrix origin, tmp,re;
-
-Matrix MatrixMul(Matrix x, Matrix y,int n,int m)
-{
-	memset(tmp.a, 0, sizeof(tmp.a));
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			for (int k = 1; k <= n; k++)
-			{
-				tmp.a[i][j] += (x.a[i][k] * y.a[k][j]);
-			}
-	return tmp;
+	
+	if (p.first + size + 1 > r || p.second + size + 1 > c)
+		return false; 
+	for (int i = 0; i < size + 1; ++i)
+		if (m[p.first + i][p.second + size] != 1 || m[p.first + size][p.second + i] != 1)
+			return false; 
+	return true;
 }
 
-Matrix IsMax(Matrix origin,int n,int m)
+int MaxSize()
 {
-	memset(re.a, 0, sizeof(re.a));
-
-
-	for (int i = 0; i<n; i++)
-		for (int j = 0; j < m; j++)
-		{
-
-		}
+	
+	vector<pair<int, int>> sp; 
+	for (int i = 0; i < r; ++i)
+		for (int j = 0; j < c; ++j)
+			if (m[i][j] == 1)
+				sp.push_back(pair<int, int>(i, j)); 
+	int MaxSize = 0; 
+	while (!sp.empty())
+	{
+		++MaxSize; 
+		vector<pair<int, int>> nsp; 
+		for (auto it : sp)
+			if (CanBeBigger(it, MaxSize))
+				nsp.push_back(it); 
+		sp = nsp; 
+	}
+	return MaxSize;
 }
+
 int main()
 {
-	int T;
-	cin >> T;
-
-	for (int i = 1; i <= T; i++)
+	int t; 
+	cin >> t; 
+	while (t-- > 0)
 	{
-		int n, m;
-		Matrix origin;
-		cin >> n >> m;
-		memset(origin.a, 0, n*m);
-		for(int i=0;i<n;i++)
-			for (int j = 0; j < m; j++)
-			{
-				int tmp;
-				cin >> tmp;
-				origin.a[i][j] = tmp;
-			}
-		MatrixMul(origin, origin, n, m);
-		for (int i = 0; i<n; i++)
-			for (int j = 0; j < m; j++)
-			{
-				cout << origin.a[i][j] << "  ";
-				if (j == m - 1)
-					cout << endl;
-			}
+		
+		
+		cin >> r >> c; 
+		for (int i = 0; i < r; ++i)
+			for (int j = 0; j < c; ++j)
+				cin >> m[i][j]; 
+		cout << MaxSize() << endl; 
 	}
 	return 0;
 }
